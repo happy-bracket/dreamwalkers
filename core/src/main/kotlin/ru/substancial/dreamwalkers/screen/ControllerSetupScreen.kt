226a916fbe3547202c3fd558d2213c1e94cac43f
happy-bracket <georgwarden@gmail.com>
2020-11-30 10:43:55 +0300

    add controllers2 dependency

--- core/src/main/kotlin/ru/substancial/dreamwalkers/screen/ControllerSetupScreen.kt
+++ core/src/main/kotlin/ru/substancial/dreamwalkers/screen/ControllerSetupScreen.kt
@@ -1,18 +1,12 @@
 package ru.substancial.dreamwalkers.screen
 
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
-import com.badlogic.gdx.controllers.Controller
-import com.badlogic.gdx.controllers.ControllerAdapter
-import com.badlogic.gdx.controllers.Controllers
-import com.badlogic.gdx.controllers.PovDirection
 import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import ru.substancial.dreamwalkers.Core
 import ru.substancial.dreamwalkers.controls.ButtonLayout
 import ru.substancial.dreamwalkers.utilities.ClearScreen
 import java.util.*
-import kotlin.math.abs
 
 class ControllerSetupScreen(val core: Core) : ScreenAdapter(), HasDisplay by HasDisplayMixin() {
 
@@ -24,40 +18,40 @@ class ControllerSetupScreen(val core: Core) : ScreenAdapter(), HasDisplay by Has
     private var cooldown: Float = 0f
 
     init {
-        Controllers.addListener(object : ControllerAdapter() {
-
-            override fun buttonUp(controller: Controller?, buttonIndex: Int): Boolean {
-                Gdx.app.log("button", buttonIndex.toString())
-                assign(buttonIndex)
-                return true
-            }
-
-            override fun axisMoved(controller: Controller?, axisIndex: Int, value: Float): Boolean {
-                Gdx.app.log("axis", "$axisIndex: $value")
-                if (abs(value) >= 0.3f) {
-                    assign(axisIndex)
-                }
-                return true
-            }
-
-            override fun povMoved(controller: Controller?, povIndex: Int, value: PovDirection?): Boolean {
-                Gdx.app.log("pov", povIndex.toString())
-                assign(povIndex)
-                return true
-            }
-
-            private fun assign(index: Int) {
-                if (cooldown >= 0.016f) return
-                cooldown = 0.5f
-                val current = assignQueue.pop()
-                ButtonLayout.buttonToIndex[current] = index
-                if (assignQueue.isEmpty()) {
-                    dispose()
-                    core.screen = GameScreen()
-                }
-            }
-
-        })
+//        Controllers.addListener(object : ControllerAdapter() {
+//
+//            override fun buttonUp(controller: Controller?, buttonIndex: Int): Boolean {
+//                Gdx.app.log("button", buttonIndex.toString())
+//                assign(buttonIndex)
+//                return true
+//            }
+//
+//            override fun axisMoved(controller: Controller?, axisIndex: Int, value: Float): Boolean {
+//                Gdx.app.log("axis", "$axisIndex: $value")
+//                if (abs(value) >= 0.3f) {
+//                    assign(axisIndex)
+//                }
+//                return true
+//            }
+//
+//            override fun povMoved(controller: Controller?, povIndex: Int, value: PovDirection?): Boolean {
+//                Gdx.app.log("pov", povIndex.toString())
+//                assign(povIndex)
+//                return true
+//            }
+//
+//            private fun assign(index: Int) {
+//                if (cooldown >= 0.016f) return
+//                cooldown = 0.5f
+//                val current = assignQueue.pop()
+//                ButtonLayout.buttonToIndex[current] = index
+//                if (assignQueue.isEmpty()) {
+//                    dispose()
+//                    core.screen = GameScreen()
+//                }
+//            }
+//
+//        })
     }
 
     override fun render(delta: Float) {
@@ -73,7 +67,7 @@ class ControllerSetupScreen(val core: Core) : ScreenAdapter(), HasDisplay by Has
     }
 
     override fun dispose() {
-        Controllers.clearListeners()
+        //Controllers.clearListeners()
         textRenderer.dispose()
         text.dispose()
     }

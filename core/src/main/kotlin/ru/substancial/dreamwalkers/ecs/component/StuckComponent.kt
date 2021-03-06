63bc4cd6684efb0beca85ab063f7d4f3b03eea80
happy-bracket <georgwarden@gmail.com>
2021-03-06 17:34:29 +0300

    implement impalement time cancel

--- core/src/main/kotlin/ru/substancial/dreamwalkers/ecs/component/StuckComponent.kt
+++ core/src/main/kotlin/ru/substancial/dreamwalkers/ecs/component/StuckComponent.kt
@@ -2,9 +2,11 @@ package ru.substancial.dreamwalkers.ecs.component
 
 import com.badlogic.ashley.core.Component
 import com.badlogic.ashley.core.Entity
+import com.badlogic.gdx.physics.box2d.Joint
 
 class StuckComponent(
         val anchor: Entity,
         val dragged: Entity,
-        var durationLeft: Float = 3f
+        var durationLeft: Float = 3f,
+        var joint: Joint? = null
 ) : Component
--- core/src/main/kotlin/ru/substancial/dreamwalkers/ecs/system/StuckSystem.kt
+++ core/src/main/kotlin/ru/substancial/dreamwalkers/ecs/system/StuckSystem.kt
@@ -2,6 +2,7 @@ package ru.substancial.dreamwalkers.ecs.system
 
 import com.badlogic.ashley.core.Engine
 import com.badlogic.ashley.core.Family
+import com.badlogic.gdx.physics.box2d.World
 import ktx.box2d.distanceJointWith
 import ru.substancial.dreamwalkers.ecs.component.BodyComponent
 import ru.substancial.dreamwalkers.ecs.component.StuckComponent
@@ -9,7 +10,9 @@ import ru.substancial.dreamwalkers.ecs.extract
 import ru.substancial.dreamwalkers.ecs.maybeExtract
 import ru.substancial.dreamwalkers.utilities.RegisteringSystem
 
-class StuckSystem : RegisteringSystem() {
+class StuckSystem(
+        private val world: World
+) : RegisteringSystem() {
 
     private val sticks by multiple(
             Family.all(StuckComponent::class.java).get()
@@ -21,18 +24,22 @@ class StuckSystem : RegisteringSystem() {
                 val sc = e.extract<StuckComponent>()
                 val anchorBody = sc.anchor.maybeExtract<BodyComponent>()?.pushbox ?: return@listener
                 val draggedBody = sc.dragged.maybeExtract<BodyComponent>()?.pushbox ?: return@listener
-                anchorBody.distanceJointWith(draggedBody) {
+                sc.joint = anchorBody.distanceJointWith(draggedBody) {
                     length = 0f
                 }
             },
-            { _, _ -> },
+            { _, e ->
+                world.destroyJoint(e.extract<StuckComponent>().joint!!)
+            },
             {}
     )
 
     override fun update(deltaTime: Float) {
         sticks.forEach {
-            var dl = it.extract<StuckComponent>().durationLeft
-            dl -= deltaTime
+            val sc = it.extract<StuckComponent>()
+            sc.durationLeft -= deltaTime
+            if (sc.durationLeft <= 0f)
+                engine.removeEntity(it)
         }
     }
 

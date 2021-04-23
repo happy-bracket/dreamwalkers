7883f679f4609c40c7a58dc622016876ccf894ec
happy-bracket <georgwarden@gmail.com>
2021-04-23 23:16:02 +0300

    add onCollisionEndComponent

---
+++ core/src/main/kotlin/ru/substancial/dreamwalkers/ecs/component/OnCollisionEndComponent.kt
@@ -0,0 +1,7 @@
+package ru.substancial.dreamwalkers.ecs.component
+
+import com.badlogic.ashley.core.Component
+
+class OnCollisionEndComponent(
+    val callbackName: String
+) : Component
--- core/src/main/kotlin/ru/substancial/dreamwalkers/ecs/system/ScenarioCollisionSystem.kt
+++ core/src/main/kotlin/ru/substancial/dreamwalkers/ecs/system/ScenarioCollisionSystem.kt
@@ -1,9 +1,9 @@
 package ru.substancial.dreamwalkers.ecs.system
 
 import com.badlogic.ashley.core.Family
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.physics.box2d.Fixture
 import ru.substancial.dreamwalkers.ecs.component.ContactComponent
+import ru.substancial.dreamwalkers.ecs.component.OnCollisionEndComponent
 import ru.substancial.dreamwalkers.ecs.component.OnCollisionStartComponent
 import ru.substancial.dreamwalkers.ecs.extract
 import ru.substancial.dreamwalkers.ecs.has
@@ -13,37 +13,49 @@ import ru.substancial.dreamwalkers.utilities.RegisteringSystem
 import ru.substancial.dreamwalkers.utilities.justListen
 
 class ScenarioCollisionSystem(
-        private val scenario: ScenarioHolder
+    private val scenario: ScenarioHolder
 ) : RegisteringSystem() {
 
     private val listener by justListen(
-            Family.all(ContactComponent::class.java).get(),
-            { e -> onBegin(e.extract()) },
-            { }
+        Family.all(ContactComponent::class.java).get(),
+        { e -> reactToContact(e.extract(), true) },
+        { e -> reactToContact(e.extract(), false) }
     )
 
-    private fun onBegin(contact: ContactComponent) {
+    private fun reactToContact(contact: ContactComponent, begin: Boolean) {
         val entityA = contact.fixtureA.body.entity ?: return
         val entityB = contact.fixtureB.body.entity ?: return
-        if ((entityA.has<OnCollisionStartComponent>()).xor(entityB.has<OnCollisionStartComponent>())) {
-            val functionName: String
-            val initiator: Fixture
-            val target: Fixture
-            if (entityA.has<OnCollisionStartComponent>()) {
+        val functionName: String
+        val initiator: Fixture
+        val target: Fixture
+        when {
+            begin && entityA.has<OnCollisionStartComponent>() -> {
                 functionName = entityA.extract<OnCollisionStartComponent>().callbackName
                 initiator = contact.fixtureB
                 target = contact.fixtureA
-            } else {
+            }
+            begin && entityB.has<OnCollisionStartComponent>() -> {
                 functionName = entityB.extract<OnCollisionStartComponent>().callbackName
                 initiator = contact.fixtureA
                 target = contact.fixtureB
             }
-            scenario.call(
-                    functionName,
-                    initiator.body.entity!!,
-                    target.body.entity!!
-            )
+            !begin && entityA.has<OnCollisionEndComponent>() -> {
+                functionName = entityA.extract<OnCollisionEndComponent>().callbackName
+                initiator = contact.fixtureB
+                target = contact.fixtureA
+            }
+            !begin && entityB.has<OnCollisionEndComponent>() -> {
+                functionName = entityB.extract<OnCollisionEndComponent>().callbackName
+                initiator = contact.fixtureA
+                target = contact.fixtureB
+            }
+            else -> return
         }
+        scenario.call(
+            functionName,
+            initiator.body.entity!!,
+            target.body.entity!!
+        )
     }
 
 }
